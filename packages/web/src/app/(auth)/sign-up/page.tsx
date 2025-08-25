728838588ba778295a8751e64fcf99d953a87825
Awel7799 <awelkassim45@gmail.com>
2025-08-25 13:49:43 -0700

    shabcn/iu installed and configured

--- packages/web/src/app/(auth)/sign-up/page.tsx
+++ packages/web/src/app/(auth)/sign-up/page.tsx
@@ -1,16 +1,15 @@
 import { Button } from "@/components/ui/button"
 import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-
 export default function CardDemo() {
   return (
     <Card className="w-full max-w-sm">
